9863b05d9d80430ba8c45d0556d807f36e2af1e2
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-13 00:31:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,10 @@
-module Findable
-  module ClassMethods
+module Concerns::Findable
 
+  def find_by_name(name)
+    self.all.detect { |i| i.name == name}
   end
 
-
+  def find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end
 end
---
+++ lib/music_library_controller.rb
@@ -0,0 +1,71 @@
+require 'pry'
+
+class MusicLibraryController
+  extend Concerns::Findable
+
+
+  def initialize(path = "./db/mp3s")
+    new_importer = MusicImporter.new(path)
+    new_importer.import
+
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    user_input = gets.strip.downcase
+    case user_input
+    when "list songs"
+      list_songs
+    when "list artists"
+      list_artists
+    when "list genres"
+      list_genres
+    when "list artist"
+      list_songs_by_artist
+    when "list genre"
+      list_songs_by_genre
+    when "play song"
+      play_song
+    when "exit"
+      'exit'
+    else
+      call
+    end
+  end
+
+  def list_songs
+    songs = Song.all.sort_by { |song| song.name }
+    
+    songs.each_with_index { |song,index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+
+  def list_artists
+
+    #Artist.all.each_with_index { |artist, index| puts "#{index + 1}. #{artist.name}"}
+  end
+
+  def list_genres
+    #Genre.all.each { |genre| puts genre.name }
+  end
+
+  def play_song
+
+  end
+
+  def list_artist
+
+  end
+
+  def list_genre
+
+  end
+
+end
