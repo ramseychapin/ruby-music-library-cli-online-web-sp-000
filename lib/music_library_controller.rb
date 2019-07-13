6f27b015a774e0b0febca6965be47aed166d2ca6
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-13 00:31:52 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
   def list_songs
     songs = Song.all.sort_by { |song| song.name }
     
-    songs.each_with_index { |song,index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    songs.each_with_index { |song,index| puts "#{songs.index(song) + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
 
   def list_artists
